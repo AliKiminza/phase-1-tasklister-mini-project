dfc85c6b188100a9505d1877b1d66e24b9f04cd6
alikiminza <ali.kiminza@student.moringaschool.com>
2023-06-24 22:12:05 +0300

    final copy

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const newButton= document.querySelector('.button')
+  newButton.addEventListener('click',(e) => {e.preventDefault();})
+
+  
+  function task(){
+    const newInput = document.querySelector('#new-task-description')
+    const textInput = newInput.value
+    const ul = document.querySelector('ul')
+    const li = document.createElement('li')
+    ul.append(li)
+    li.innerText= textInput
+    console.log(textInput)
+  }
+  newButton.addEventListener('click',task);
 });
